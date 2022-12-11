82ecab422cdd41a7e7ce25703d8f1cc2421f1db8
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-12-10 22:51:08 -0800

    get from state

--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,11 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 import SingleArticle from '../SingleArticle';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { loadArticles } from '../../store/articleReducer';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import articleReducer, { loadArticles } from '../../store/articleReducer';
 
 const ArticleList = () => {
   const dispatch = useDispatch();
+  const articles = useSelector(state => state.articleState.entries);
+
   useEffect(() => {
     dispatch(loadArticles());
   }, [dispatch]);
@@ -14,11 +16,9 @@ const ArticleList = () => {
     <div>
       <h1>Article List</h1>
       <ol>
-        <li>Gilligan's Island. Is it true?</li>
-        <li>A Baseball Moment</li>
-        <li>Poke Moment</li>
-        <li>Cool Cats</li>
-        <li>Why Am I At Home</li>
+        {articles.map(article => (
+          <li key={article.id}>{article.title}</li>
+        ))}
       </ol>
 
       <Switch>
